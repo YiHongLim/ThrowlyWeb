f18d909c8eb17841aeeb543729aa98ee8544cf8c
Yi Hong Lim <limyihong1415@gmail.com>
2025-09-09 12:02:24 -0500

    Refactor folder structure and initialized new project with npm packages

---
+++ throwly_web_app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import CampaignDetails from "./pages/GoFurnishMe/CampaignDetails";
+
+function App() {
+  return (
+      
+    <div className="App">
+      <header className="App-header">
+          <CampaignDetails />
+      </header>
+    </div>
+  );
+}
+
+export default App;
