b1abeaf5eca3e88df03beed3eaad6893559a4ec9
Yi Hong Lim <limyihong1415@gmail.com>
2025-09-18 12:24:02 -0500

    Added react router dom

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// @ts-ignore
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home/Home';
+import CampaignPage from './pages/Go_Furnish_Me/CampaignPage';
+import GoFurnishMePage from "./pages/Go_Furnish_Me/GoFurnishMePage";
+import CampaignPageDetails from "./pages/Go_Furnish_Me/CampaignPageDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      <Home />
-      </header>
-    </div>
+    <Router>
+        <Routes>
+            <Route path="/" element={<CampaignPage />} />
+            <Route path="/go-furnish-me" element={<GoFurnishMePage />} />
+            <Route path="/campaign-page-details" element={<CampaignPageDetails />} />
+        </Routes>
+    </Router>
   );
+
 }
 
 export default App;
