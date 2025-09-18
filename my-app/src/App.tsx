4fc559d1ade4d2d4696182cad807c51cee85be5d
Yi Hong Lim <limyihong1415@gmail.com>
2025-09-18 13:26:48 -0500

    Add link to go-furnish-me page on Navbar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -16,7 +16,6 @@ function App() {
             <Route path="/go-furnish-me" element={<GoFurnishMePage />} />
             <Route path="/campaign-page-details" element={<CampaignPageDetails />} />
             <Route path="/campaign-page" element={<CampaignPage />} />
-            <Route path="/campaign-page" element={<CampaignPage />} />
         </Routes>
     </Router>
   );
