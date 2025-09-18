99d60d24fca1b6d9655c0cd59fd7c87805f76d63
Noop04 <anoop04@iastate.edu>
2025-09-18 13:42:19 -0500

    cleared

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 // @ts-ignore
 import logo from './logo.svg';
-import './App.css';
+import './output.css';
 import CampaignPage from './pages/Go_Furnish_Me/CampaignPage';
 import GoFurnishMePage from "./pages/Go_Furnish_Me/GoFurnishMePage";
 import CampaignPageDetails from "./pages/Go_Furnish_Me/CampaignPageDetails";
@@ -15,17 +15,15 @@ import CartPage from './pages/CartPage';
 
 function App() {
   return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/go-furnish-me" element={<GoFurnishMePage />} />
-            <Route path="/campaign-page-details" element={<CampaignPageDetails />} />
-            <Route path="/campaign-page" element={<CampaignPage />} />
-            <Route path="/login" element={<AuthPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/cart" element={<CartPage />} />
-        </Routes>
-    </Router>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/go-furnish-me" element={<GoFurnishMePage />} />
+        <Route path="/campaign-page-details" element={<CampaignPageDetails />} />
+        <Route path="/campaign-page" element={<CampaignPage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/cart" element={<CartPage />} />
+    </Routes>
 
   );
 }
