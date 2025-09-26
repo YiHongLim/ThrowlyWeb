c660508252fea40615ede012dfa281165e5d8de7
Yi Hong Lim <limyihong1415@gmail.com>
2025-09-26 11:58:37 -0500

    Refactored to comment out wavy SVG divider

--- my-app/src/pages/Go_Furnish_Me/CampaignPage.tsx
+++ my-app/src/pages/Go_Furnish_Me/CampaignPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CampaignHero from '../../components/go_furnish_me/CampaignHero';
 import CampaignCards from "../../components/go_furnish_me/CampaignCards";
 import {useNavigate} from "react-router";
+import {bgImage, moving} from "../../assets/images/gofurnishme_images"
 import {Divider} from "antd";
 
 function CampaignPage() {
@@ -14,20 +15,21 @@ function CampaignPage() {
                     subtitle="Discover, fund, and furnish spaces with ease."
                     ctaText="Start a GoFurnishMe"
                     onCtaClick={() => navigate('/go-furnish-me')}
-                    imageUrl="/logo512.png"
+                    imageUrl={moving}
+                    bgUrl={bgImage}
                 />
-                <div style={{ margin: "40px 0", width: "100%" }}>
-                    <svg viewBox="0 0 1440 100" width="100%" height="100" preserveAspectRatio="none">
-                        <path d="M0,30 C360,90 1080,0 1440,60 L1440,100 L0,100 Z" fill="#d44b4f">
-                            <animate attributeName="d" values="
-        M0,30 C360,90 1080,0 1440,60 L1440,100 L0,100 Z;
-        M0,50 C400,0 1040,100 1440,30 L1440,100 L0,100 Z;
-        M0,30 C360,90 1080,0 1440,60 L1440,100 L0,100 Z
-      " dur="8s" repeatCount="indefinite" />
-                        </path>
-                    </svg>
-                </div>
-
+      {/*          <div style={{ margin: "40px 0", width: "100%" }}>*/}
+      {/*              <svg viewBox="0 0 1440 100" width="100%" height="100" preserveAspectRatio="none">*/}
+      {/*                  <path d="M0,30 C360,90 1080,0 1440,60 L1440,100 L0,100 Z" fill="#d44b4f">*/}
+      {/*                      <animate attributeName="d" values="*/}
+      {/*  M0,30 C360,90 1080,0 1440,60 L1440,100 L0,100 Z;*/}
+      {/*  M0,50 C400,0 1040,100 1440,30 L1440,100 L0,100 Z;*/}
+      {/*  M0,30 C360,90 1080,0 1440,60 L1440,100 L0,100 Z*/}
+      {/*" dur="8s" repeatCount="indefinite" />*/}
+      {/*                  </path>*/}
+      {/*              </svg>*/}
+      {/*          </div>*/}
+                <Divider />
                 <CampaignCards />
             </main>
             <footer className="bg-gray-100 p-4 text-center">
