c3d11483a7a34eb94f03bbfa201cb519a5671afa
Yi Hong Lim <limyihong1415@gmail.com>
2025-10-09 09:42:16 -0500

    Created DonationListPage to view individual campaign donations

---
+++ my-app/src/utils/firebaseUtils.ts
@@ -0,0 +1,23 @@
+import {doc, getDoc} from "firebase/firestore";
+import {db} from "../firebase";
+import {DonationItem} from "./types";
+
+export const fetchDonorNames = async (donations: DonationItem[]): Promise<DonationItem[]> => {
+    const userIds = donations
+        .map(d => d.userId)
+        .filter((id, index, arr) => !!id && arr.indexOf(id) === index);
+    const userPromises = userIds.map(uid => getDoc(doc(db, "Users", uid)));
+    const userSnaps = await Promise.all(userPromises);
+    const userMap: Record<string, { firstName?: string; lastName?: string }> = {};
+    userSnaps.forEach(snap => {
+        if (snap.exists()) {
+            userMap[snap.id] = snap.data();
+        }
+    });
+    // Now, enrich your items
+    return donations.map(d => ({
+        ...d,
+        firstName: userMap[d.userId]?.firstName || "",
+        lastName: userMap[d.userId]?.lastName || "",
+    }));
+};
