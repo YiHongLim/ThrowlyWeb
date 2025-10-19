79308cc98efaf75c0ce194c1d8317d3e357b09b8
Yi Hong Lim <limyihong1415@gmail.com>
2025-10-19 11:21:48 -0500

    Remove web folder (accidental copy)

--- web/my-app/src/utils/helpers.ts
+++
@@ -1,6 +0,0 @@
-import {categories} from "../data/categories";
-
-export const getCategoryLabel = (id: string): string | undefined => {
-    const found = categories.find(cat => cat.value === id);
-    return found?.label;
-}
